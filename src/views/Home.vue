dcdd118aca497c42ec6ad7367bba6f550071f6a5
INGANZAMARUMPU <jonkur96@gmail.com>
2022-07-13 11:46:44 +0200

    added top menus

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,27 @@
     <ion-header>
       <ion-toolbar>
         <ion-title>SHAPP</ion-title>
+        <ion-buttons slot="secondary">
+          <ion-button @click="showSearch">
+            <ion-icon slot="icon-only" :icon="getIcon('search')"/>
+          </ion-button>
+          <ion-button id="menu-toggler">
+            <ion-icon slot="icon-only"
+              :ios="getIcon('ellipsisHorizontal')"
+              :md="getIcon('ellipsisVertical')"/>
+          </ion-button>
+        </ion-buttons>
       </ion-toolbar>
+      <ion-popover trigger="menu-toggler" dismiss-on-select="true" show-backdrop="false">
+        <ion-content>
+          <ion-list lines="none">
+            <ion-item button @click="logOut">
+              <ion-label>Deconnexion</ion-label>
+              <ion-icon :src="getIcon('powerOutline')"/>
+            </ion-item>
+          </ion-list>
+        </ion-content>
+      </ion-popover>
     </ion-header>
     <ion-content class="ion-padding">
       <h1>SHAPP</h1>
@@ -15,7 +35,13 @@
 </template>
 
 <script>
-
 export default {
+  data(){
+    return {}
+  },
+  methods:{
+    showSearch(){},
+    addTodo(){}
+  }
 }
 </script>
