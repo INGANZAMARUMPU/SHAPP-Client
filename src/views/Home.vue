7570ac885e0f3aac7486a4bfcff5e69cab035dc7
INGANZAMARUMPU <jonkur96@gmail.com>
2022-07-13 11:32:04 +0200

    added fab button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,10 +4,13 @@
       <ion-toolbar>
         <ion-title>SHAPP</ion-title>
       </ion-toolbar>
-      <ion-content class="ion-padding">
-        <h1>SHAPP</h1>
-      </ion-content>
     </ion-header>
+    <ion-content class="ion-padding">
+      <h1>SHAPP</h1>
+      <ion-fab-button class="todo-fab" @click="addTodo">
+        <ion-icon :src="getIcon('add')"></ion-icon>
+      </ion-fab-button>
+    </ion-content>
   </ion-page>
 </template>
 
