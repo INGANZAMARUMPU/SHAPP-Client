0256e0aac57ae1ef26c23b198633d2e62f82c7f6
INGANZAMARUMPU <jonkur96@gmail.com>
2022-07-22 13:55:38 +0200

    coded qr generation

--- src/components/dialog_scan_results.vue
+++ src/components/dialog_scan_results.vue
@@ -2,6 +2,15 @@
   <div class="dialog" v-if="active">
     <div class="body ion-padding">
       <h3>Evenement</h3>
+      <div class="qr">
+        <img id="qr_img"/>
+      </div>
+      <div class="descr">
+        <div>Nom de la place</div>
+        <h3>{{ item.nom }}</h3>
+        <div>Numero de la place</div>
+        <h3>{{ item.no }}</h3>
+      </div>
       <ion-col class="options">
         <ion-button fill=clear @click="close">Laisser</ion-button>
       </ion-col>
@@ -10,6 +19,7 @@
 </template>
 
 <script>
+import QRCode from 'qrcode'
 export default {
   props: {
     active:{type:Boolean, required:true},
@@ -17,6 +27,11 @@ export default {
   },
   data(){
     return {
+    }
+  },
+  computed:{
+    qr_data(){
+      return JSON.stringify(this.item)
     }
   },
   watch:{
@@ -30,6 +45,16 @@ export default {
     close(){
       this.$emit("close")
     },
+    generateQRs(){
+      let qr_img = document.getElementById("qr_img");
+      let data = btoa(this.qr_data)
+      QRCode.toDataURL(data).then(src => {
+        qr_img.setAttribute("src", src)
+      })
+    }
+  },
+  mounted(){
+    setTimeout(this.generateQRs, 1000)
   }
 };
 </script>
